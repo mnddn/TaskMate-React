de7ae21a375c4b39224ce8ba953db9eefa35c5c3
Moin Munna <mnddnmunna@gmail.com>
2025-09-11 00:49:06 +0600

    Full commit of the taskmate  app

---
+++ src/components/AddTask.js
@@ -0,0 +1,47 @@
+export const AddTask = ({ taskList, setTaskList, task, setTask }) => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (task.id) {
+      const date = new Date();
+      const updatedTaskList = taskList.map((todo) =>
+        todo.id === task.id
+          ? {
+              id: task.id,
+              name: e.target.task.value,
+              time: `${date.toLocaleTimeString()} ${date.toLocaleDateString()}`,
+            }
+          : todo
+      );
+      setTaskList(updatedTaskList);
+      setTask({});
+    } else {
+      const date = new Date();
+      const newTask = {
+        id: date.getTime(),
+        name: e.target.task.value,
+        time: `${date.toLocaleTimeString()} ${date.toLocaleDateString()}`,
+      };
+      setTaskList([...taskList, newTask]);
+      setTask({});
+    }
+    e.target.task.value = "";
+  };
+
+  return (
+    <section className="addTask">
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="task"
+          autoComplete="off"
+          placeholder="add task"
+          maxLength="25"
+          value={task.name || ""}
+          onChange={(e) => setTask({ ...task, name: e.target.value })}
+        />
+        <button type="submit">{task.id ? "Update" : "Add"}</button>
+      </form>
+    </section>
+  );
+};
